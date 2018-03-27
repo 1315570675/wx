1ae64cf88556a2cdd6b082d3a99139d76aa01bf2
yutongman <18256991570@163.com>
2018-03-27 18:17:36 +0800

    add:项目添加

---
+++ douban/pages/index/index.js
@@ -0,0 +1,86 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    movies:[],
+    page: 1,
+    size: 6,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.loadMovies();
+  },
+
+  loadMovies(){
+    const {size,page} = this.data;
+
+    wx.request({
+      url: `https://db.miaov.com/doubanapi/v0/movie/list?page=${page}&size=${size}`,
+      success:(res) =>{
+       const {data} = res.data;
+       const movies = this.data.movies;
+       console.log(data.length)
+       for(let i=0; i<data.length; i+=2){
+         movies.push([data[i],data[i+1] ? data[i+1] : null]);
+       }
+       console.log(movies)
+        this.setData({movies})
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
